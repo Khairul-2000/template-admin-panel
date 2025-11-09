7b87d4fac942d5b7f8016932a2ba94748adacc99
Khairul-2000 <kahirulislam135@gmail.com>
2025-11-09 15:36:18 +0600

    Remove unused JSON files and related API functions to streamline codebase

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -87,29 +87,7 @@ export const useUsers = ({ page = 1, limit = 10 }) => {
   return { users, isLoading, isError, error, refetch };
 };
 
-// get all payments
-export const useStripePayments = ({ page = 1, limit = 10 }) => {
-  const getData = async () => {
-    const response = await API.get(
-      `/admin/stripe-payments/?page=${page}&limit=${limit}`
-    );
 
-    return response.data;
-  };
-
-  const {
-    data: stripePayments = [],
-    isLoading,
-    isError,
-    error,
-    refetch,
-  } = useQuery({
-    queryKey: ["stripePayments", page, limit],
-    queryFn: getData,
-  });
-
-  return { stripePayments, isLoading, isError, error, refetch };
-};
 
 // get all admin
 export const useAllAdmins = () => {
@@ -132,26 +110,6 @@ export const useAllAdmins = () => {
   return { allAdmins, isLoading, isError, error, refetch };
 };
 
-// get all orbit post
-export const useAllOrbitPost = () => {
-  const getData = async () => {
-    const response = await axios.get("/orbitPost.json");
-    return response.data;
-  };
-
-  const {
-    data: allOrbit = null,
-    isLoading,
-    isError,
-    error,
-    refetch,
-  } = useQuery({
-    queryKey: ["allOrbit"],
-    queryFn: getData,
-  });
-
-  return { allOrbit, isLoading, isError, error, refetch };
-};
 
 // administrators
 export const getMockAdministrators = async () => {
@@ -160,98 +118,9 @@ export const getMockAdministrators = async () => {
   return response.data;
 };
 
-// payments
-export const getMockPayments = async ({ page = 1, limit = 10 }) => {
-  const res = await axios.get("/payments.json");
-  const allData = res.data || [];
 
-  // Fake pagination
-  const totalPayments = allData.length;
-  const totalPages = Math.ceil(totalPayments / limit);
-  const paginatedPayments = allData.slice((page - 1) * limit, page * limit);
 
-  return {
-    data: paginatedPayments,
-    pagination: {
-      totalPayments,
-      page,
-      limit,
-      totalPages,
-    },
-  };
-};
 
-// get message
-export const getMockMessages = async () => {
-  const response = await axios.get("/user_chat.json");
-
-  return response.data;
-};
-
-// get previous scans
-export const getMockPreviousScans = async () => {
-  const response = await axios.get("/previousScans.json");
-
-  return response.data;
-};
-
-// get Single scans
-export const getMockSingleScans = async () => {
-  const response = await axios.get("/singleScan.json");
-
-  return response.data;
-};
-
-// get Saved Recipes
-export const getMockSavedRecipes = async () => {
-  const response = await axios.get("/recipes.json");
-
-  return response.data;
-};
-
-// users list for messages
-export const getMockUsersForMessages = async () => {
-  const response = await axios.get("/messageUserList.json");
-
-  return response.data;
-};
-
-export const useUsersForMessage = () => {
-  const {
-    data: usersForMessage = [],
-    isLoading,
-    isError,
-    error,
-    refetch,
-  } = useQuery({
-    queryKey: ["usersForMessage"],
-    queryFn: getMockUsersForMessages,
-  });
-
-  return { usersForMessage, isLoading, isError, error, refetch };
-};
-
-// community
-export const getMockCommunity = async () => {
-  const response = await axios.get("/user_messages.json");
-
-  return response.data;
-};
-
-export const useCommunityMessages = () => {
-  const {
-    data: communityMessage = [],
-    isLoading,
-    isError,
-    error,
-    refetch,
-  } = useQuery({
-    queryKey: ["communityMessage"],
-    queryFn: getMockCommunity,
-  });
-
-  return { communityMessage, isLoading, isError, error, refetch };
-};
 
 
 
